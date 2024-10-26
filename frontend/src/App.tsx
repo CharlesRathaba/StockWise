87ddfe6301a1d4574d10c53cd97b9402c19c9e5a
Charles Rathaba <rathabacharles@gmail.com>
2024-10-27 00:08:24 +0200

    dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LandingPage from './components/LandingPage';
+import LandingPage from './components/landing/LandingPage';
 import logo from './logo.svg';
 import './App.css';
 
